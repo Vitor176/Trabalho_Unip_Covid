9a1302fe0fa02e26da753c881dd0a6b4d241786e
JulianaViccari <juliana.z.viccari@gmail.com>
2021-11-20 00:29:08 -0300

    initialcommit

--- login.c
+++ login.c
@@ -2,23 +2,23 @@
 #include <string.h>
 
 char login[15] = "admin";
-char login1[15]; 
+char login1[15];
 char senha[15] = "1234";
 char senha1[15];
 //-- Determina as Contas nos "", tendo em conta a quantidade de caracteres dentro dos []
-
+//oi
 main()
 {
 
-printf("LOGIN: ");
-scanf("%s",login1); 
-//-- Onde será inserido o nome 
-printf("Senha: ");
-scanf("%s",senha1); 
-//-- Onde será inserido a senha
+    printf("LOGIN: ");
+    scanf("%s", login1);
+    //-- Onde será inserido o nome
+    printf("Senha: ");
+    scanf("%s", senha1);
+    //-- Onde será inserido a senha
 
     //-- validaçao para saber se o login está correto
-    if (strcmp(login, login1) || strcmp(senha, senha1) == 0) 
+    if (strcmp(login, login1) || strcmp(senha, senha1) == 0)
     {
         printf("Logado\n");
     }
@@ -26,6 +26,4 @@ scanf("%s",senha1);
     {
         printf("Usuario Invalido. Tente novamente!\n");
     }
-
-
 }
