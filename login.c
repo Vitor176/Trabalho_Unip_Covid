f51b6053fb3d142485dfb3065d9eeedc9f4966db
JulianaViccari <juliana.z.viccari@gmail.com>
2021-11-21 17:12:18 -0300

    Alteracoes estrutura do programa

--- login.c
+++ login.c
@@ -1,85 +1,157 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <conio.h>
 #define SIZE 200
 
-char nome[SIZE][50];
-int cpf[SIZE][11];
-char email[SIZE][50];
-char endereco[SIZE][50];
-int cep[SIZE][20];
-int op;
-void cadastro();
-
 char login[15] = "medico";
-char login1[15];
 char senha[15] = "1234";
-char senha1[15];
+int logado;
+
+char nome[SIZE][50];
+int idade[SIZE];
+char cpf[SIZE][12];
+char email[SIZE][50];
+int op;
 //-- Determina as Contas nos "", tendo em conta a quantidade de caracteres dentro dos []
 
-main(void)
+void autenticacao();
+void cadastro();
+void menu();
+void listar();
+
+int main(void)
 {
-    FILE *Ponteiro;
-    Ponteiro = fopen("arquivo.txt", "a");
-    // Ponteiro = (int*) malloc(100*sizeof(int));
 
-    printf("LOGIN: ");
-    scanf("%s", login1);
-    //-- Onde será inserido o nome
-    printf("Senha: ");
-    scanf("%s", senha1);
-    //-- Onde será inserido a senha
+    autenticacao();
 
-    //-- validaçao para saber se o login está correto
-    if (strcmp(login, login1) || strcmp(senha, senha1) == 0)
+    if (logado == 1)
     {
-        printf("Logado\n");
-        cadastro();
+        menu();
+        listar();
     }
     else
     {
-        printf("Usuario Invalido. Tente novamente!\n");
-        return 1;
+        return 0;
     }
 
-    printf("Dados gravados com sucesso!");
+    return 0;
+}
 
-    return (0);
+void autenticacao()
+{
+
+    system("cls");
+
+    int opLogin;
+    char login1[15];
+    char senha1[15];
+
+    printf("LOGIN: ");
+    scanf("%s", &login1);
+    //-- Onde será inserido o nome
+    printf("Senha: ");
+    scanf("%s", &senha1);
+    //-- Onde será inserido a senha
+
+    //-- validaçao para saber se o login está correto
+    if (strcmp(login, login1) == 0 && strcmp(senha, senha1) == 0)
+    {
+        printf("Usuário autenticado com sucesso!\n");
+        logado = 1;
+    }
+    else
+    {
+        printf("Usuario Inválido.\n1 - Para tentar novamente \n2 - Para sair!\n");
+        scanf("%d", &opLogin);
+
+        if (opLogin == 1)
+        {
+            autenticacao();
+        }
+        else if (opLogin == 2)
+        {
+            logado = 0;
+            return;
+        }
+    }
+}
+
+void menu()
+{
+
+    do
+    {
+        printf("\n1 - Cadastrar Paciente\n2 - Listar Paciente\n0 - Sair\n");
+        scanf("%d", &op);
+
+        switch (op)
+        {
+        case 1:
+            cadastro();
+            break;
+
+        case 2:
+            listar();
+            break;
+        }
+
+    } while (op != 0);
 }
 
 void cadastro()
 {
+    FILE *Arquivo;
+    Arquivo = fopen("arquivo.txt", "a");
+
     static int linha;
 
     do
     {
-        FILE *Ponteiro;
-        Ponteiro = fopen("arquivo.txt", "a");
 
-        printf("\nDigite o Nome do paciente:");
-        scanf("%[^\n]s", &nome[linha], stdin);
-        fprintf(Ponteiro, "\nNome: %s", nome);
+        system("cls");
 
-        printf("Digite o CPF do paciente:");
-        scanf("%[^\n]s", &cpf[linha]);
-        fprintf(Ponteiro, "\nCPF: %s", cpf);
+        printf("\n---- Cadastrando cliente -----\n");
+        printf("\nDigite o nome:");
+        scanf("%s", &nome[linha]);
 
-        printf("\nDigite o email do paciente:");
-        scanf("%[^\n]s", &email[linha]);
-        fprintf(Ponteiro, "\nEmail: %s", email);
+        printf("\nDigite o e-mail:");
+        scanf("%s", &email[linha]);
 
-        printf("\nDigite o endereço do paciente:");
-        scanf("%[^\n]s", &endereco[linha]);
-        fprintf(Ponteiro, "\nEndereco: %s", endereco);
+        printf("\nDigite a idade:");
+        scanf("%d", &idade[linha]);
 
-        printf("\nDigite o CEP do paciente:");
-        scanf("%[^\n]d", &cep[linha]);
-        fprintf(Ponteiro, "\nCep: %d", cep);
+        printf("\nDigite o cpf:");
+        scanf("%s", &cpf[linha]);
 
-        fprintf(Ponteiro, "\n");
-        printf("\nDigite 1 para continuar ou outro valor para sair ");
-        scanf("%[^\n]d", &op);
+        printf("\n1 - Para continuar\n2 -Para sair\n");
+        scanf("%d", &op);
+
+        fprintf(Arquivo, "%s %s %d %s\n", nome[linha], email[linha], idade[linha], cpf[linha]);
+
         linha++;
-        fclose(Ponteiro);
+
     } while (op == 1);
+
+    fclose(Arquivo);
+}
+
+void listar()
+{
+    system("cls");
+
+    printf("\n Pascientes cadastrados\n");
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (idade[i] == 0)
+        {
+            break;
+        }
+        printf("\n Paciente : %d\n", i + 1);
+        printf("Nome: %s\n", nome[i]);
+        printf("Idade: %d\n", idade[i]);
+        printf("Email: %s\n", email[i]);
+        printf("CPf: %s\n", cpf[i]);
+        printf("\n----------------\n");
+    }
 }
